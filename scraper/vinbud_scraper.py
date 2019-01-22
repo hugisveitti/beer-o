d4c9a239fd187598bb88e7658b3dc4a6f71e8634
mombanger69 <hugiholm1@gmail.com>
2019-01-22 17:57:06 +0000

    scraper

--- scraper/vinbud_scraper.py
+++ scraper/vinbud_scraper.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import re
-import pandas as pd
-import os
 import json
-
 from time import sleep
 
 
